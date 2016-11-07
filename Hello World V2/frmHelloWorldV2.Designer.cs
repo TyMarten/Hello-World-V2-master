e5d31b9626088c786872de93e55c7165938340aa
TyMarten <Tyamarten@gmail.com>
2016-11-07 08:00:53 -0600

    hello world v2
    very annoying computer talks back to you

---
+++ Hello World V2/frmHelloWorldV2.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Hello_World_V2
+{
+    partial class frmHelloWorldV2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmHelloWorldV2));
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.btnSpeak = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.BackColor = System.Drawing.Color.Transparent;
+            this.lblOutput.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutput.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblOutput.Location = new System.Drawing.Point(105, 88);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(179, 119);
+            this.lblOutput.TabIndex = 0;
+            this.lblOutput.Click += new System.EventHandler(this.txtOutput_Click);
+            // 
+            // btnSpeak
+            // 
+            this.btnSpeak.Location = new System.Drawing.Point(147, 304);
+            this.btnSpeak.Name = "btnSpeak";
+            this.btnSpeak.Size = new System.Drawing.Size(75, 23);
+            this.btnSpeak.TabIndex = 1;
+            this.btnSpeak.Text = "Speak";
+            this.btnSpeak.UseVisualStyleBackColor = true;
+            this.btnSpeak.Click += new System.EventHandler(this.btnSpeak_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(90, 278);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(194, 20);
+            this.txtInput.TabIndex = 2;
+            // 
+            // frmHelloWorldV2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(374, 339);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnSpeak);
+            this.Controls.Add(this.lblOutput);
+            this.Name = "frmHelloWorldV2";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Button btnSpeak;
+        private System.Windows.Forms.TextBox txtInput;
+    }
+}
+
---
+++ Hello World V2/frmHelloWorldV2.cs
@@ -0,0 +1,80 @@
+﻿/**
+
+ *
+
+ * Name: Tyler Marten    
+
+ * Teacher: Mr. Hardman
+
+ * Assignment 2, Program ?
+
+ * Date Last Modified: 10/14/16
+
+ *
+
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hello_World_V2
+{
+    public partial class frmHelloWorldV2 : Form
+    {
+        private static string previousUserAnswer;
+
+        //creates a new version of the frm can be used to change to robots question
+        public frmHelloWorldV2()
+        {
+            InitializeComponent();
+            lblOutput.Text = "Hello there... ummmm whats your name again";
+        }
+        //puts the new question in the question box (output)
+        public frmHelloWorldV2(string answer,string newQuestion)
+        {
+            InitializeComponent();
+            previousUserAnswer = answer;
+            lblOutput.Text = newQuestion;
+        }
+
+        private void txtOutput_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnSpeak_Click(object sender, EventArgs e)
+        {
+            string currentUserAnswer = txtInput.Text;
+            //checks if the output (the robots question) is still Hello there... ummmm whats your name again
+            if ( (lblOutput.Text).Equals("Hello there... ummmm whats your name again") == true)
+            {
+                //Second Time the frm is used, then gets the users name then closes the frm
+                frmHelloWorldV2 secondTime = new frmHelloWorldV2(currentUserAnswer, currentUserAnswer + " Of course, What is the square root of 841. ");
+                secondTime.Show();
+                this.Hide();
+            }
+
+            if ((lblOutput.Text).Equals( previousUserAnswer + " Of course, What is the square root of 841. ") == true) 
+            {
+                if (currentUserAnswer == 29)
+                {
+                    frmHelloWorldV2 thirdTime = new frmHelloWorldV2(currentUserAnswer, "Wow your a nerd");
+
+                } else {
+                    frmHelloWorldV2 thirdTime = new frmHelloWorldV2(currentUserAnswer, "Not right, so you have a life then -_-");
+                    this.Close();
+                }
+
+
+            }
+
+            this.Close();
+        }
+    }
+}
